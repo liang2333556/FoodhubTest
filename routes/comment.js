95e81f198fe11809d668b7e0e4eac6d951676c91
Ang Li <20086443@mail.wit.ie>
2019-11-03 15:44:47 +0000

    fix the warning and make the code coverage reporting

--- routes/comment.js
+++ routes/comment.js
@@ -1,53 +1,53 @@
-let express = require('express');
-let router = express.Router();
-let mongoose = require('mongoose');
-var Comment= require('../models/comment');
-var Essay= require('../models/essay');
+let express = require("express")
+let router = express.Router()
+let mongoose = require("mongoose")
+var Comment= require("../models/comment")
+var Essay= require("../models/essay")
 
-var mongodbUri = 'mongodb+srv://leon:liang369369@wit-donation-cluster-lovf9.mongodb.net/foodhub?retryWrites=true&w=majority'
-mongoose.connect(mongodbUri);
+var mongodbUri = "mongodb+srv://leon:liang369369@wit-donation-cluster-lovf9.mongodb.net/foodhub?retryWrites=true&w=majority"
+mongoose.connect(mongodbUri)
 
-let db = mongoose.connection;
+let db = mongoose.connection
 
-db.on('error', function (err) {
-    console.log('Unable to Connect to [ ' + db.name + ' ]', err);
-});
+db.on("error", function (err) {
+  console.log("Unable to Connect to [ " + db.name + " ]", err)
+})
 
-db.once('open', function () {
-    console.log('Successfully Connected to [ ' + db.name + ' ]');
-});
+db.once("open", function () {
+  console.log("Successfully Connected to [ " + db.name + " ]")
+})
 
 
 router.addComment = (req, res) => {
 
-    res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json")
 
-    var comment = new Comment();
-     comment.author=req.body.author,
-     comment.content=req.body.content,
-     comment.essay_id=req.body.essay_id,
+  var comment = new Comment()
+  comment.author=req.body.author,
+  comment.content=req.body.content,
+  comment.essay_id=req.body.essay_id,
 
 
-      comment.save(function(err) {
+  comment.save(function(err) {
+    if (err)
+      res.json({ message: "New comment NOT Added!", errmsg : err } )
+    else
+      res.json({ message: "Comment Successfully Added!", data: comment })
+  })
+
+  Essay.findById(comment.essay_id, function(err,essay) {
+    if (err)
+      res.json({ message: "Fail to submit!", errmsg : err } )
+    else {
+      essay.comment.push({content: comment.content, comment_id: comment.id,author:comment.author})
+      essay.save(function (err) {
         if (err)
-            res.json({ message: 'New comment NOT Added!', errmsg : err } );
+          res.json({ message: "Fail to submit!", errmsg : err } )
         else
-            res.json({ message: 'Comment Successfully Added!', data: comment });
-    });
-
-    Essay.findById(comment.essay_id, function(err,essay) {
-        if (err)
-            res.json({ message: 'Fail to submit!', errmsg : err } );
-        else {
-            essay.comment.push({content: comment.content, comment_id: comment.id,author:comment.author});
-            essay.save(function (err) {
-                if (err)
-                    res.json({ message: 'Fail to submit!', errmsg : err } );
-                else
-                    res.json({ message: 'Submit your comment Successfully!', data: essay});
-            });
-        }
-    });
+          res.json({ message: "Submit your comment Successfully!", data: essay})
+      })
+    }
+  })
 }
 
 
@@ -58,4 +58,4 @@ router.addComment = (req, res) => {
 
 
 
-module.exports = router;
+module.exports = router
--- routes/userEssay.js
+++ routes/userEssay.js
@@ -1,89 +1,89 @@
-let express = require('express');
-let router = express.Router();
-let mongoose = require('mongoose');
-var Essay= require('../models/essay');
-var mongodbUri = 'mongodb+srv://leon:liang369369@wit-donation-cluster-lovf9.mongodb.net/foodhub?retryWrites=true&w=majority'
-mongoose.connect(mongodbUri,{useNewUrlParser:true});
+let express = require("express")
+let router = express.Router()
+let mongoose = require("mongoose")
+var Essay= require("../models/essay")
+var mongodbUri = "mongodb+srv://leon:liang369369@wit-donation-cluster-lovf9.mongodb.net/foodhub?retryWrites=true&w=majority"
+mongoose.connect(mongodbUri,{useNewUrlParser:true})
 
-let db = mongoose.connection;
+let db = mongoose.connection
 
-db.on('error', function (err) {
-    console.log('Unable to Connect to [ ' + db.name + ' ]', err);
-});
+db.on("error", function (err) {
+  console.log("Unable to Connect to [ " + db.name + " ]", err)
+})
 
-db.once('open', function () {
-    console.log('Successfully Connected to [ ' + db.name + ' ]');
-});
+db.once("open", function () {
+  console.log("Successfully Connected to [ " + db.name + " ]")
+})
 
 router.findAll = (req, res) => {
-    // Return a JSON representation of our list
-    res.setHeader('Content-Type', 'application/json');
+  // Return a JSON representation of our list
+  res.setHeader("Content-Type", "application/json")
 
-    Essay.find(function(err, essay) {
-        if (err)
-            res.send(err);
+  Essay.find(function(err, essay) {
+    if (err)
+      res.send(err)
 
-        res.send(JSON.stringify(essay,null,5));
-    });
+    res.send(JSON.stringify(essay,null,5))
+  })
 }
 router.findOne = (req, res) => {
 
-    res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json")
 
-    Essay.find({ "_id" : req.params.id },function(err,essay) {
-        if (err)
-            res.json({ message: 'Essay NOT Found!', errmsg : err } );
-        else
-            res.send(JSON.stringify(essay,null,5));
-    });
+  Essay.find({ "_id" : req.params.id },function(err,essay) {
+    if (err)
+      res.json({ message: "Essay NOT Found!", errmsg : err } )
+    else
+      res.send(JSON.stringify(essay,null,5))
+  })
 }
 router.addEssay = (req, res) => {
 
-    res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json")
 
-    var essay = new Essay();
+  var essay = new Essay()
 
-    essay.author = req.body.author;
-    essay.content= req.body.content;
-    essay.comment=req.body.comment;
-    essay.date=new Date();
-    essay.likes=req.body.likes;
-    essay.save(function(err) {
-        if (err)
-            res.json({ message: 'New essay NOT Added!', errmsg : err } );
-        else
-            res.json({ message: 'Essay Successfully Added!', data: essay });
-    });
+  essay.author = req.body.author
+  essay.content= req.body.content
+  essay.comment=req.body.comment
+  essay.date=new Date()
+  essay.likes=req.body.likes
+  essay.save(function(err) {
+    if (err)
+      res.json({ message: "New essay NOT Added!", errmsg : err } )
+    else
+      res.json({ message: "Essay Successfully Added!", data: essay })
+  })
 }
 
 router.incrementLikes = (req, res) => {
 
-    Essay.findById(req.params.id, function(err,essay) {
+  Essay.findById(req.params.id, function(err,essay) {
+    if (err)
+      res.json({ message: "Fail to submit!", errmsg : err } )
+    else {
+      essay.likes += 1
+      essay.save(function (err) {
         if (err)
-            res.json({ message: 'Fail to submit!', errmsg : err } );
-        else {
-            essay.likes += 1;
-            essay.save(function (err) {
-                if (err)
-                    res.json({ message: 'Fail to submit!', errmsg : err } );
-                else
-                    res.json({ message: 'Submit your like Successfully!', data: essay});
-            });
-        }
-    });
+          res.json({ message: "Fail to submit!", errmsg : err } )
+        else
+          res.json({ message: "Submit your like Successfully!", data: essay})
+      })
+    }
+  })
 }
 
 
 router.deleteEssay = (req, res) => {
 
-    Essay.findByIdAndRemove(req.params.id, function(err) {
-        if (err)
-            res.json({ message: 'Essay NOT DELETED!', errmsg : err } );
-        else
-            res.json({ message: 'Essay Successfully Deleted!'});
-    });
+  Essay.findByIdAndRemove(req.params.id, function(err) {
+    if (err)
+      res.json({ message: "Essay NOT DELETED!", errmsg : err } )
+    else
+      res.json({ message: "Essay Successfully Deleted!"})
+  })
 }
 
 
 
-module.exports = router;
+module.exports = router
